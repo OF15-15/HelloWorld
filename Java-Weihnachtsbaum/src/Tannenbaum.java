e9e4d2f22f96be1453ef3699c0b63baa9da4a764
ich <ich@hildes-imac.fritz.box>
2021-05-04 16:08:55 +0200

    Deco added

--- Java-Weihnachtsbaum/src/Tannenbaum.java
+++ Java-Weihnachtsbaum/src/Tannenbaum.java
@@ -1,16 +1,28 @@
+import java.util.Random;
 
 public class Tannenbaum {
 
 	public static void main(String[] args) {
-		int breite = 90;
+		int breite = 9;
 		int leerzeichen = (breite - 1) / 2;
 		int blaetter = 1;
+		Random wuerfel = new Random();
+		int zufallszahl;
+
 		while (blaetter <= breite) {
 			for (int i = 0; i < leerzeichen; i++) {
 				System.out.print(" ");
 			}
 			for (int i = 0; i < blaetter; i++) {
-				System.out.print("1");
+				zufallszahl = wuerfel.nextInt(10);
+				if (zufallszahl == 0)
+					System.out.print("x");
+				else {
+					if (zufallszahl == 1)
+						System.out.print("O");
+					else
+						System.out.print("1");
+				}
 			}
 			System.out.println();
 			leerzeichen--;
@@ -19,6 +31,6 @@ public class Tannenbaum {
 		for (int i = 0; i < breite / 2; i++) {
 			System.out.print(" ");
 		}
-		System.out.println("O");
+		System.out.println("H");
 	}
 }
