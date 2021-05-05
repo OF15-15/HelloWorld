3dabb1111729085ce1ab2e9043aefa11c1870f6c
ich <ich@hildes-imac.fritz.box>
2021-05-05 08:35:45 +0200

    Refactoring

--- Java-Weihnachtsbaum/src/Tannenbaum.java
+++ Java-Weihnachtsbaum/src/Tannenbaum.java
@@ -2,35 +2,54 @@ import java.util.Random;
 
 public class Tannenbaum {
 
-	public static void main(String[] args) {
-		int breite = 9;
-		int leerzeichen = (breite - 1) / 2;
-		int blaetter = 1;
-		Random wuerfel = new Random();
-		int zufallszahl;
+	static int breite = 9;
+	static int leerzeichen = (breite - 1) / 2;
+	static int blaetter = 1;
+	static Random wuerfel = new Random();
+	static int zufallszahl;
 
+	public static void zeichneBaum() {
 		while (blaetter <= breite) {
-			for (int i = 0; i < leerzeichen; i++) {
-				System.out.print(" ");
-			}
-			for (int i = 0; i < blaetter; i++) {
-				zufallszahl = wuerfel.nextInt(10);
-				if (zufallszahl == 0)
-					System.out.print("x");
-				else {
-					if (zufallszahl == 1)
-						System.out.print("O");
-					else
-						System.out.print("1");
-				}
-			}
+			zeichneLeerzeichen();
+			zeichneBlaetter();
+
 			System.out.println();
 			leerzeichen--;
 			blaetter = blaetter + 2;
 		}
+		zeichneStamm();
+	}
+
+	public static void zeichneLeerzeichen() {
+		for (int i = 0; i < leerzeichen; i++) {
+			System.out.print(" ");
+		}
+	}
+
+	public static void zeichneBlaetter() {
+		for (int i = 0; i < blaetter; i++) {
+			zufallszahl = wuerfel.nextInt(10);
+			if (zufallszahl == 0)
+				System.out.print("x");
+			else {
+				if (zufallszahl == 1)
+					System.out.print("O");
+				else
+					System.out.print("1");
+			}
+		}
+	}
+
+	public static void zeichneStamm() {
 		for (int i = 0; i < breite / 2; i++) {
 			System.out.print(" ");
 		}
 		System.out.println("H");
+
+	}
+
+	public static void main(String[] args) {
+		zeichneBaum();
+
 	}
 }
